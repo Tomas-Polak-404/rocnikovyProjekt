5f8c87301cbe99eaab0cb1423d1d5c6fb895b5c0
Tomáš Polák <tomaspolak404@gmail.com>
2024-05-22 21:50:00 +0200

    PlayerDeathAndGems

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement; // Nutné pro práci se scénami
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public float hp = 100; // Zdraví hráče
     public float maxHealth;
     public Image healthBar;
+
+    public Image gemBar; // Referenci na Image komponentu GemBar
+    private int gemCount = 0;
+    private int totalGems = 3; // Celkový počet drahokamů
+
+    public GameObject gameOverScreen; // Reference na Game Over obrazovku
 
     private void Start()
     {
@@ -15,27 +21,61 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        
         healthBar.fillAmount = Mathf.Clamp(hp / maxHealth, 0, 1);
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Zkontrolujeme, zda se hráč dotkl objektu s tagem "portal"
-        if (other.CompareTag("Portal"))
+        // Zkontrolujeme, zda se hráč dotkl objektu s tagem "Portal"
+        if (other.CompareTag("Portal") && gemCount >= totalGems)
         {
             // Spustíme přechod na scénu "lvl2v2"
             SceneManager.LoadScene("lvl2v2");
         }
+
+        // Zkontrolujeme, zda se hráč dotkl objektu s tagem "Gem"
+        if (other.CompareTag("Gem"))
+        {
+            gemCount++;
+            UpdateGemBar();
+            Destroy(other.gameObject); // Znič drahokam
+        }
+
+        // Zkontrolujeme, zda se hráč dotkl objektu s tagem "Void"
+        if (other.CompareTag("Void"))
+        {
+            // Nastavení HP na 0
+            hp = 0;
+            healthBar.fillAmount = 0;
+
+            // Zobrazení Game Over obrazovky
+            gameOverScreen.SetActive(true);
+
+            // Deaktivace pohybu hráče
+            GetComponent<PlayerMovement>().enabled = false;
+        }
+    }
+
+
+    private void UpdateGemBar()
+    {
+        float fillAmount = (float)gemCount / totalGems;
+        gemBar.fillAmount = fillAmount;
     }
 
     // Metoda pro přijetí poškození
     public void TakeDamage(int damage)
     {
-
         hp -= damage; // Odečtení poškození od zdraví hráče
-        if (hp < 0)
+        if (hp <= 0)
         {
             hp = 0; // Zajišťuje, že hráč nemůže mít méně než 0 HP
+
+            // Zastavení veškerého pohybu hráče
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.velocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+            GetComponent<PlayerMovement>().enabled = false;
         }
     }
 }
